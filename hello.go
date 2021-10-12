ec9d0c79a2604ed1d1460ba39c9f35ed4c283b37
fengmingxing <1181214772@qq.com>
2021-10-12 15:59:21 +0800

    Update hello.go
    demo

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ func main() {
  r := gin.Default()
  r.GET("/ping", func(c *gin.Context) {
      c.JSON(200, gin.H{
-         "message": "hello CP VKE!!!",
+         "message": "hello VKE!!!",
      })
  })
  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
