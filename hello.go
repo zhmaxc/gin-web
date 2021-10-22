8a875a9aa3c6f92fb53d6b96d8e55452b33aa73e
fengmingxing <1181214772@qq.com>
2021-10-22 13:15:42 +0800

    Update hello.go
    update

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ func main() {
  r := gin.Default()
  r.GET("/ping", func(c *gin.Context) {
      c.JSON(200, gin.H{
-         "message": "hello CP 20211021 and test 4444444 站内信通知44443333 !!!",
+         "message": "hello CP 20211022 and test 55555 站内信通知44443333 !!!",
      })
  })
  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
